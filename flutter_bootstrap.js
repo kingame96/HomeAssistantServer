3e8dcdbd0245256086a2cbc171ceb0027bcbbb48
kingame <truongthinh619@gmail.com>
2026-06-17 19:56:00 +0700

    First committ

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "387317901" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3937210600" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
